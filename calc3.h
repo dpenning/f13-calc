9e481c6595b868f0db011820e8f29e6ae06f1c36
Mike Roup <roup.michael@gmail.com>
2013-11-23 14:12:30 -0500

    Change tabs to 2spaces

--- calc3.h
+++ calc3.h
@@ -2,37 +2,37 @@ typedef enum { typeCon, typeFloat, typeId, typeOpr } nodeEnum; /* added */
 
 /* constants */
 typedef struct {
-    int value;                  /* value of constant */
+  int value;                  /* value of constant */
 } conNodeType;
 
 /* floats - added */
 typedef struct {
-    float value;                /* value of float */
+  float value;                /* value of float */
 } floatNodeType;
 
 /* identifiers */
 typedef struct {
-    char *s;	                /* variable name - changed JWJ */
+  char *s;                  /* variable name - changed JWJ */
 } idNodeType;
 
 /* operators */
 typedef struct {
-    int oper;                   /* operator */
-    int nops;                   /* number of operands */
-    struct nodeTypeTag *op[1];  /* operands (expandable) */
+  int oper;                   /* operator */
+  int nops;                   /* number of operands */
+  struct nodeTypeTag *op[1];  /* operands (expandable) */
 } oprNodeType;
 
 typedef struct nodeTypeTag {
-    nodeEnum type;              /* type of node */
+  nodeEnum type;              /* type of node */
 
-    /* union must be last entry in nodeType */
-    /* because operNodeType may dynamically increase */
-    union {
-        conNodeType con;        /* constants */
-        floatNodeType fl;       /* floats */
-        idNodeType id;          /* identifiers */
-        oprNodeType opr;        /* operators */
-    };
+  /* union must be last entry in nodeType */
+  /* because operNodeType may dynamically increase */
+  union {
+    conNodeType con;        /* constants */
+    floatNodeType fl;       /* floats */
+    idNodeType id;          /* identifiers */
+    oprNodeType opr;        /* operators */
+  };
 } nodeType;
 
 #define TYPE_INT   1
