5a181c0622f0d24e547c192c8bdb05d6e8dd3011
Mike Roup <roup.michael@gmail.com>
2013-12-12 21:20:39 -0500

    change assembly instructions to lowercase

--- calc3p.c
+++ calc3p.c
@@ -76,22 +76,22 @@ int ex(nodeType *p,int build) {
 
   switch(p->type) {
     case typeCon:
-      printf("%04d I_CONSTANT value:%d\n", lbl++, p->con.value); 
+      printf("%04d I_Constant value:%d\n", lbl++, p->con.value); 
       return TYPE_INT;
     case typeFloat:
-      printf("%04d R_CONSTANT value:%f\n", lbl++, p->fl.value); 
+      printf("%04d R_Constant value:%f\n", lbl++, p->fl.value); 
       return TYPE_FLOAT;
     case typeId:
       sym = getSymbolEntry(p->id.s);
       if (sym->type == TYPE_INT) {
-        printf("%04d I_VARIABLE lev:%d disp:%d\n"
+        printf("%04d I_Variable lev:%d disp:%d\n"
              , lbl++
              , sym->blk_level
              , sym->offset); 
         return TYPE_INT;
       }
       else {
-        printf("%04d R_VARIABLE lev:%d disp:%d\n"
+        printf("%04d R_Variable lev:%d disp:%d\n"
              , lbl++
              , sym->blk_level
              , sym->offset); 
@@ -153,37 +153,37 @@ int ex(nodeType *p,int build) {
         return 0;
       case '=':
         ex(p->opr.op[1],1);
-        printf("%04d I_ASSIGN words:%d\n", lbl++, 1);
+        printf("%04d I_Assign words:%d\n", lbl++, 1);
         return 0;
       case UMINUS:
         ex(p->opr.op[0],1);
-        printf("%04d I_MINUS", lbl++);
+        printf("%04d I_Minus", lbl++);
         break;
       default:
         ex(p->opr.op[0],1);
         ex(p->opr.op[1],1);
         switch(p->opr.oper) {
-        case '+':   printf("%04d I_ADD", lbl++); break;
-        case '-':   printf("%04d I_SUBTRACT", lbl++); break; 
-        case '*':   printf("%04d I_MULTIPLY", lbl++); break;
-        case '/':   printf("%04d I_DIVIDE", lbl++); break;
-        case '<':   printf("%04d I_LESS", lbl++); break;
-        case '>':   printf("%04d I_GREATER", lbl++); break;
-        case EQ:    printf("%04d I_EQUAL", lbl++); break;
+        case '+':   printf("%04d I_Add", lbl++); break;
+        case '-':   printf("%04d I_Subtract", lbl++); break; 
+        case '*':   printf("%04d I_Multiply", lbl++); break;
+        case '/':   printf("%04d I_Divide", lbl++); break;
+        case '<':   printf("%04d I_Less", lbl++); break;
+        case '>':   printf("%04d I_Greater", lbl++); break;
+        case EQ:    printf("%04d I_Equal", lbl++); break;
         case NE:
-          printf("%04d I_EQUAL", lbl++);
-          printf("%04d I_CONSTANT value:0", lbl++);
-          printf("%04d I_EQUAL", lbl++);
+          printf("%04d I_Equal", lbl++);
+          printf("%04d I_Constant value:0", lbl++);
+          printf("%04d I_Equal", lbl++);
           break;
         case GE:
-          printf("%04d I_LESS", lbl++);
-          printf("%04d I_CONSTANT value:0", lbl++);
-          printf("%04d I_EQUAL", lbl++);
+          printf("%04d I_Less", lbl++);
+          printf("%04d I_Constant value:0", lbl++);
+          printf("%04d I_Equal", lbl++);
           break;
         case LE:
-          printf("%04d I_GREATER", lbl++);
-          printf("%04d I_CONSTANT value:0", lbl++);
-          printf("%04d I_EQUAL", lbl++);
+          printf("%04d I_Greater", lbl++);
+          printf("%04d I_Constant value:0", lbl++);
+          printf("%04d I_Equal", lbl++);
           break;
         }
       }
