f16aec8ad338f0fd6b3f435f697b8b2405732e0d
dpenning <dpenning1@gmail.com>
2013-12-16 00:57:45 -0500

    fixed some stuff in the test file

--- TESTS/real_test.sh
+++ TESTS/real_test.sh
@@ -2,28 +2,26 @@
 
 
 
-
-echo ""
-echo "NFM Program"
-echo ""
-
-./calc3p < $1
-echo "  --Program--"
-./pstack/apd calc_out
-echo "  --Program--"
-./pstack/api calc_out
-
-echo ""
-echo "Our Program"
-echo ""
-
-./mycalc3 < $1 > wow.aa
-echo "  --Program--"
-more wow.aa
-echo "  --Program--"
-./pstack/apm < wow.aa > wow.apm
-./pstack/api wow
-
-echo ""
-rm wow.aa
-rm wow.apm
+for f in tests/*.calc
+do
+	echo ""
+	echo "NFM Program"
+	echo ""
+	./calc3p < $f
+	echo "  --Program--"
+	./pstack/apd calc_out
+	echo "  --Program--"
+	./pstack/api calc_out
+	echo ""
+	echo "Our Program"
+	echo ""
+	./mycalc3 < $f > wow.aa
+	echo "  --Program--"
+	more wow.aa
+	echo "  --Program--"
+	./pstack/apm < wow.aa > wow.apm
+	./pstack/api wow
+	echo ""
+	rm wow.aa
+	rm wow.apm
+done
